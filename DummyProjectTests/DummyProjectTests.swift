e9a8d4272f0d6488f76331a3e7fb95ab7fae8368
Hilal Hakani <hilalalhakani@gmail.com>
2024-01-15 20:24:43 +0200

    Added tests

---
+++ DummyProjectTests/DummyProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  DummyProjectTests.swift
+//  DummyProjectTests
+//
+//  Created by HH on 15/01/2024.
+//
+
+import XCTest
+
+final class DummyProjectTests: XCTestCase {
+
+    func test() {
+        XCTAssertEqual(1, 1)
+    }
+
+}
