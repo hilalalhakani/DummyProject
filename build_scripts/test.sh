616c9815789a59b7f53bb1470e4e817c3dd03f99
Hilal Hakani <hilalalhakani@gmail.com>
2024-01-15 22:57:22 +0200

    Added chmod

--- build_scripts/test.sh
+++ build_scripts/test.sh
@@ -8,7 +8,6 @@ if $HAS_PACKAGES_CACHE_HIT; then
   set -o pipefail && xcrun xcodebuild test \
     -scheme DummyProject \
     -project DummyProject.xcodeproj \
-    -destination 'platform=iOS Simulator,name=iPhone 14' \
     -skipPackageUpdates \
     -disableAutomaticPackageResolution \
     -clonedSourcePackagesDirPath "$PACKAGES_PATH" |
@@ -19,7 +18,6 @@ else
   set -o pipefail && xcrun xcodebuild test \
     -scheme DummyProject \
     -project DummyProject.xcodeproj \
-    -destination 'platform=iOS Simulator,name=iPhone 14' \
     -disableAutomaticPackageResolution \
     -clonedSourcePackagesDirPath "$PACKAGES_PATH" |
     xcbeautify --renderer github-actions
