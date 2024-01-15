b415eda45dd41031449ebee6929197ff234febb9
Hilal Hakani <hilalalhakani@gmail.com>
2024-01-15 20:12:29 +0200

    Initial Commit

---
+++ DummyProject/DummyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyProjectApp.swift
+//  DummyProject
+//
+//  Created by HH on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct DummyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
